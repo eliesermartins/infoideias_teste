0e34e54e947235f5c018a9e2a503d27f7e464533
eliesermartins <ejmartins81@gmail.com>
2022-10-31 17:03:17 -0300

    Teste

---
+++ funcoes.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace SRC;
+
+class Funcoes
+{
+    /*
+
+    Desenvolva uma função que receba como parâmetro o ano e retorne o século ao qual este ano faz parte. O primeiro século começa no ano 1 e termina no ano 100, o segundo século começa no ano 101 e termina no 200.
+
+	Exemplos para teste:
+
+	Ano 1905 = século 20
+	Ano 1700 = século 17
+
+     * */
+    public function SeculoAno(int $ano): int {
+        return is_int($ano/100) ?  $ano/100 : intval($ano/100)+1;
+     }
+    }
+
+    
+	
+	
+	
+	
+	
+	
+	
+	/*
+
+    Desenvolva uma função que receba como parâmetro um número inteiro e retorne o numero primo imediatamente anterior ao número recebido
+
+    Exemplo para teste:
+
+    Numero = 10 resposta = 7
+    Número = 29 resposta = 23
+
+     * */
+    public function PrimoAnterior(int $numero): int {
+			$numero=$numero-1;
+			$divisores = 0;
+  
+			while($divisores==0){
+				$divisores = ePrimo($numero);
+				if($divisores>0)
+					return $numero;
+				$numero=$numero-1;
+			}
+    }
+	
+	 
+	 public function ePrimo(int $valor): int {
+		if ($valor == 1)
+		return 0;
+		for ($i = 2; $i <= $valor/2; $i++){
+			if ($valor % $i == 0)
+				return 0;
+		}
+		return 1;
+	}
+  
+
+    /*
+
+    Desenvolva uma função que receba como parâmetro um array multidimensional de números inteiros e retorne como resposta o segundo maior número.
+
+    Exemplo para teste:
+
+	Array multidimensional = array (
+	array(25,22,18),
+	array(10,15,13),
+	array(24,5,2),
+	array(80,17,15)
+	);
+
+	resposta = 25
+
+     * */
+    public function SegundoMaior(array $arr): int {
+		$lista =  array();
+		foreach ($arr as $value) {
+			foreach ($value as $result) {
+			array_push($lista,$result);
+			}
+		}
+		rsort($lista);
+		echo $lista[1] ;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+    /*
+   Desenvolva uma função que receba como parâmetro um array de números inteiros e responda com TRUE or FALSE se é possível obter uma sequencia crescente removendo apenas um elemento do array.
+
+	Exemplos para teste 
+
+	Obs.:-  É Importante  realizar todos os testes abaixo para garantir o funcionamento correto.
+         -  Sequencias com apenas um elemento são consideradas crescentes
+
+    [1, 3, 2, 1]  false
+    [1, 3, 2]  true
+    [1, 2, 1, 2]  false
+    [3, 6, 5, 8, 10, 20, 15] false
+    [1, 1, 2, 3, 4, 4] false
+    [1, 4, 10, 4, 2] false
+    [10, 1, 2, 3, 4, 5] true
+    [1, 1, 1, 2, 3] false
+    [0, -2, 5, 6] true
+    [1, 2, 3, 4, 5, 3, 5, 6] false
+    [40, 50, 60, 10, 20, 30] false
+    [1, 1] true
+    [1, 2, 5, 3, 5] true
+    [1, 2, 5, 5, 5] false
+    [10, 1, 2, 3, 4, 5, 6, 1] false
+    [1, 2, 3, 4, 3, 6] true
+    [1, 2, 3, 4, 99, 5, 6] true
+    [123, -17, -5, 1, 2, 3, 12, 43, 45] true
+    [3, 5, 67, 98, 3] true
+
+     * */
+    
+	public function SequenciaCrescente(array $arr): boolean {
+		
+		$arrUnico = array_unique($arr);
+   		if(count($arrUnico)==1)
+   			return true;
+   		
+   		if( $arr===$arrUnico){
+   	
+		for ($x=0; $x<count($arr); $x++) {
+			$lista =  $arr;
+			unset($lista[$x]);
+			$lista = array_values($lista);
+			$listaOrdenada = array_values($lista);
+			sort($listaOrdenada);
+			if($lista===$listaOrdenada)
+				return true;
+		}
+   		}
+	return false;
+    }
+}
